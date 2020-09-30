49225afba73dc043bce15e4d8a573099ee62790c
Taylor Arnold <taylorj.arnold2013@hotmail.com>
2020-09-29 19:25:37 -0700

    PyBank.py

--- 03-Python-Challenge/PyBank/PyBank.py
+++ 03-Python-Challenge/PyBank/PyBank.py
@@ -1,52 +1,51 @@
-# Importing the Library Folders
 import os
 import csv
-
-# Creating the path for PyBank CSV
-csvpath = os.path.join("..", "Resources", "budget_data.csv")
-with open('budget_data.csv', 'r') as csvfile:
-        csvreader = csvreader(csvfile, delimiter = ',')
-        csv_header = next(csvfile)
-
-# Setting the PyBank Variables and creating an empty list to add the Budget_Data.csv Values 
-
-total_months = 0
-total_profit_loss = 0
-total_differences = 0
-greatest_monthly_profit = 0
-lowest_monthly_profit = 0
-
-#  add the values to the empty CSV list
-
-for row in csvreader:
-        total_months.append(row[0])
-        total_profit_loss.append(int(row[1]))
-for i in range(len(profit)-1):
+file = os.path.join('..', 'Resources', 'budget_data.csv')
+with open('budget_data.csv','r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    header = next(csvreader)
+    #create empty lists to add the csv values to 
+    month_count = []
+    profit = []
+    change_profit = []
+    
+                      
+    #iterate through the values and add them to the empty list 
+    for row in csvreader:
+        month_count.append(row[0])
+        profit.append(int(row[1]))
+    for i in range(len(profit)-1):
         change_profit.append(profit[i+1]-profit[i])
+                      
+#evaluate the max and min from the list made
+increase = max(change_profit)
+decrease = min(change_profit)
 
-# Understanding the min and max profits from the csv file
-greatest_increase_profit = max(total_differences)
-lowest_monthly_profit = min(total_differences)
+#using the index, 
+month_increase = change_profit.index(max(change_profit))+1
+month_decrease = change_profit.index(min(change_profit))+1
 
-# Understanding the min and max of the dates 
-increase = date[total_months.index(max(greatest_increase_profit))]
-decrease = date[total_months.index(min(lowest_monthly_profit))]
-
-# Summary of Financial Analysis 
 
 print("Financial Analysis")
-print(f"Total Months:{len(total_months)}")
-print(f"Total: ${sum(total_profit_loss)}")
-print(f"Average Change: {round(sum(total_differences)/len(total_profit_loss),2)}")
-print(f"Greatest Increase in Profits: {total_months[month_increase]} (${(str(increase))})")
-print(f"Greatest Decrease in Profits: {total_months[month_decrease]} (${(str(decrease))})")    
+print("------------------------")
+print(f"Total Months:{len(month_count)}")
+print(f"Total: ${sum(profit)}")
+print(f"Average Change: {round(sum(change_profit)/len(change_profit),2)}")
+print(f"Greatest Increase in Profits: {month_count[month_increase]} (${(str(increase))})")
+print(f"Greatest Decrease in Profits: {month_count[month_decrease]} (${(str(decrease))})")      
 
-# Output Text file
 output = os.path.join(".", 'output.txt')
-with open(output, "w") as new:
-output.txt("Financial Analysis")
-output.txt(f"Total Months:{len(total_months)}")
-output.txt(f"Total: ${sum(total_profit_loss)}")
-output.txt(f"Average Change: {round(sum(total_differences)/len(total_profit_loss),2)}")
-output.txt(f"Greatest Increase in Profits: {total_months[month_increase]} (${(str(increase))})")
-output.txt(f"Greatest Decrease in Profits: {total_months[month_decrease]} (${(str(decrease))})")  
+with open(output,"w") as new:
+    new.write("Financial Analysis")
+    new.write("\n")
+    new.write("------------------------")
+    new.write("\n")
+    new.write(f"Total Months:{len(month_count)}")
+    new.write("\n")
+    new.write(f"Total: ${sum(profit)}")
+    new.write("\n")
+    new.write(f"Average Change: {round(sum(change_profit)/len(change_profit),2)}")
+    new.write("\n")
+    new.write(f"Greatest Increase in Profits: {month_count[month_increase]} (${(str(increase))})")
+    new.write("\n")
+    new.write(f"Greatest Decrease in Profits: {month_count[month_decrease]} (${(str(decrease))})")
