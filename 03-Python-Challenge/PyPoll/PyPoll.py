36b8e985bd9d4160cb2a4d18f552c1296e18d86b
Taylor Arnold <taylorj.arnold2013@hotmail.com>
2020-09-29 18:23:11 -0700

    03-Python-Challenge/

---
+++ 03-Python-Challenge/PyPoll/PyPoll.py
@@ -0,0 +1,11 @@
+# Importing the Library Folders
+import os
+import csv
+
+# Creating the path for PyBank 
+csvpath = os.path.join("..", "Resources", "election_data.csv")
+ with open('budget')
+
+
+
+# Set the PyBank Variables
