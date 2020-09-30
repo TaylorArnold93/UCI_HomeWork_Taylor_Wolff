340c2d25db77a6982e6fc6ebcf73ad66beda18d0
Taylor Arnold <taylorj.arnold2013@hotmail.com>
2020-09-29 19:53:17 -0700

    PyBank/

--- 03-Python-Challenge/PyBank/PyBank.py
+++ 03-Python-Challenge/PyBank/PyBank.py
@@ -3,27 +3,32 @@ import os
 import csv
 
 # Creating the path for PyBank CSV
-csvpath = os.path.join("..", "Resources", "budget_data.csv")
-with open('budget_data.csv', 'r') as csvfile:
-        csv_reader = csv_reader(csvfile, delimiter = ',')
-        csv_header = next(csvfile)
+csv_path = os.path.join("Resources", "budget_data.csv")
+with open('csv_path', 'r') as csv_file:
+        open('output_analysis.txt' 'w') as text_file:
+
+        budget_data = csv.reader(csv_file, delimiter = ',')
+        budget_data_header = next(csv_file)
 
 # Setting the PyBank Variables and creating an empty list to add the Budget_Data.csv Values 
 
-total_months = 0
-total_profit_loss = 0
-total_differences = 0
-greatest_monthly_profit = 0
-lowest_monthly_profit = 0
+total_months = list 0
+total_profit_loss = list 0
+average_profit_loss_date = dict 0
+greatest_monthly_profit = dict 0
+lowest_monthly_profit = dict 0
+i = 0
 
 #  add the values to the empty CSV list
 
-for row in csv_reader:
+for row in budget_data:
         total_months.append(row[0])
         total_profit_loss.append(int(row[1]))
 
-for i in range(len(profit)-1):
-        total_differences.append(total_profit_loss[i+1]-total_profit_loss[i])
+        if i = 0
+                average_profit_loss_date[0] = total_months[i]
+        else 
+                average_profit_loss_date[total_profit_loss[i]] - total_profit_loss[i-1]] = total_months[i]
 
 # Understanding the min and max profits from the csv file
 greatest_increase_profit = max(total_differences)
