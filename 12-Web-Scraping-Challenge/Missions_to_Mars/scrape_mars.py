a5e0e5010b713df3047daf4ba5a35d16d00a25a0
Taylor Arnold <taylorj.arnold2013@hotmail.com>
2020-12-05 11:12:13 -0800

    chrome

--- 12-Web-Scraping-Challenge/Missions_to_Mars/scrape_mars.py
+++ 12-Web-Scraping-Challenge/Missions_to_Mars/scrape_mars.py
@@ -1,18 +1,2 @@
-import pymongo
-import requests
-from splinter import Browser
-from bs4 import BeautifulSoup as bs
-import pandas as pd
-import time
 
-# connecting to the mongodb
-client = pymongo.MongoClient('mongodb://localhost:27017')
-db = client.mars_db
-collection = db.mars 
-
-# execute the browser path
-def init_browser():
-    # @NOTE: Replace the path with your actual path to the chromedriver
-    executable_path = {'executable_path': 'chromedriver.exe'}
-    return Browser('chrome', **executable_path, headless=False)
 
