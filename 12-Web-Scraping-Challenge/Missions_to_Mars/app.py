aeef11b78874f39f43f2406898bc189bf120ef0c
Taylor Arnold <taylorj.arnold2013@hotmail.com>
2020-12-05 13:27:06 -0800

    new changes

--- 12-Web-Scraping-Challenge/Missions_to_Mars/app.py
+++ 12-Web-Scraping-Challenge/Missions_to_Mars/app.py
@@ -1,8 +1,7 @@
 # import libraries
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import pandas as pd 
-import pymongo
 import scrape_mars
 
 # create Flask app
@@ -13,24 +12,16 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/app.py")
 
 # create route that for the index.html template
 @app.route("/")
-def echo():
-     # Find data
-    m_facts = mongo.db.collection.find_one()
-    #Return templated data
-    return render_template("index.html", mars=m_facts)
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars = mars)
 
 @app.route("/scrape")
 def scrape():
-    # Run the scrape function
-    mars_data = scrape_mars.scrape_all()
-
-    # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
-
-    # Redirect back to home page
+    mars = mongo.db.mars 
+    mars_table_data = scrape_mars.scrape()
+    mars.update({}, mars_data, upsert=True)
     return redirect("/", code=302)
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
