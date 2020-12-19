a2d05863e40d7afdc0974c3a0956f3e2fc9033f5
Taylor Arnold <taylorj.arnold2013@hotmail.com>
2020-12-19 10:25:57 -0800

    updated 2

--- 13-Intro to JS Homework/StarterCode/static/js/app.js
+++ 13-Intro to JS Homework/StarterCode/static/js/app.js
@@ -5,10 +5,10 @@ const tbody= d3.select('tbody');
 // YOUR CODE HERE!
 
 
-// Two functions (1) for that click (2) populate table with that data
+// Building the table into 2 parts
 
 function buildTable(data){ 
-    // First, we need to clear the table, making space for the new data
+    // First, needing the clear table, and making space for the new data
     tbody.html("");
 
     data.forEach((item) => {
@@ -20,42 +20,42 @@ function buildTable(data){
 };
 
 function handleClick() {
-    // Grab the date time value from the filer
+    // Looking through the date time value from the filer
     const date = d3.select("#datetime").property("value");
     const city = d3.select("#city").property("value");
     const state = d3.select("#state").property("value");
     const country = d3.select("#country").property("value");
     const shape = d3.select("#shape").property("value");
 
-    // Let a new variable (filteredData) be equal to our tabledata (we essentially have a copy of the original table)
+    // New variable (filteredData) be equal to our tabledata
     var filteredData= tableData; 
 
     // console.log(filteredData.datetime)
-    // // Check to see if a date was entered and filter the data using that date
+    // // Make surte to see a date was entered and we can filter the data using that date.
     if (date) {
-        // Rebuild the table using the filter data and produce on the page
+        // Being able to utilize the table to filter data and view on the html index
         var filteredData = filteredData.filter(info => info.datetime === date);
     };
     
     if (city) {
         var lowCity= city.toLowerCase();
-        // Rebuild the table using the filter data and produce on the page
+        // Being able to utilize the table to filter data and view on the html index
         var filteredData = filteredData.filter(info => info.city === lowCity);
     };
     
     if (state) {
         var lowState= state.toLowerCase();
-        // Rebuild the table using the filter data and produce on the page
+        // Being able to utilize the table to filter data and view on the html index
         var filteredData = filteredData.filter(info => info.state === lowState);
     };
     if (country) {
         var lowCountry= country.toLowerCase();
-        // Rebuild the table using the filter data and produce on the page
+        // Being able to utilize the table to filter data and view on the html index
         var filteredData = filteredData.filter(info => info.country === lowCountry);
     };
     if (shape) {
         var lowShape= shape.toLowerCase();
-        // Rebuild the table using the filter data and produce on the page
+        // Being able to utilize the table to filter data and view on the html index
         var filteredData = filteredData.filter(info => info.shape === lowShape);
     };
 
