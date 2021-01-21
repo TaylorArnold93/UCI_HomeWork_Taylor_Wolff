0bfca5af38bdb8a60c208912d7a79b50594b28ed
Taylor Arnold <taylorj.arnold2013@hotmail.com>
2021-01-20 22:05:04 -0800

    commit

--- 16-Mapping-Web/static/js/logic.js
+++ 16-Mapping-Web/static/js/logic.js
@@ -1,101 +1,139 @@
-//level 1
+// USGS endpoint for 'All Earthqukes from the Past 7 Days'
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-//url to GeoJSON. see https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php for more info
-url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+// Pull data from USGS endpoint and call .features 
+d3.json(queryUrl, function(data) {
+    // Call createFeatures with data.features
+    createFeatures(data.features);
+    // console.log(data.features);
+}); 
 
-//function to build map
-function mapDisplay () {
+// Create function to create features for data.features
+function createFeatures(earthquakeDataFeatures){
 
-    //layers
+    function onEachFeature(feature, circle) {
+        // if statement to filter out any null values
+        if (feature.properties.place && feature.properties.time && feature.properties.mag && feature.geometry.coordinates[1] && feature.geometry.coordinates[0] && feature.geometry.coordinates[2] !== null) {            
+        
+            // Set up pop up with place, mag, depth, and time data
+            circle.bindPopup("<h3>" + feature.properties.place + "<h3><p>" +
+            feature.properties.mag + " magnitudes</p><p>" + feature.geometry.coordinates[2] +
+            " depth</p><p>" + new Date(feature.properties.time) + "</p>");   
+        }      
+    }    
+    // Create function for radius to return mag * 2000 for better visualization
+    function radiusMag(mag) { 
+        return mag * 25000;
+    }
 
-// Create the map object with options
-  var earthQuakeMap = L.map("map", {
-    center: [0, 0],
-    zoom: 3,
-    worldCopyJump: true
-  });
+    // Create function for color scale base on depth value
+    function circleColor(depth) {
+        // if statement to state range and colors
+        if (depth  < -10) {
+            return "#fffb2"
+        }
+        else if (depth < 10) {
+            return "#fed976"
+        }
+        else if (depth < 30) {
+            return "#feb24c"
+        }
+        else if (depth < 50) {
+            return "#fd8d3c"
+        }
+        else if (depth < 70) {
+            return "#f03b20"
+        }
+        else if (depth < 90) {
+            return "#bd0026"
+        }
+    }
+    // Create a GeoJSON layer containing the features array on the earthquakeData object
+    // Run the onEachFeature function once for each piece of data in the array
+    var earthquakes = L.geoJSON(earthquakeDataFeatures, {
+        // function to create cirlces
+        pointToLayer: function(earthquakeDataFeatures, latlng) {
+            // Create circles
+            return L.circle(latlng, {
+                // Set raidus to magnitude value
+                radius: radiusMag(earthquakeDataFeatures.properties.mag),
+                // Set colors to depth
+                color: circleColor(earthquakeDataFeatures.geometry.coordinates[2]),
+                fillColor: circleColor(earthquakeDataFeatures.geometry.coordinates[2]),
+                fillOpacity: 0.7
+            });
+        },
+        onEachFeature: onEachFeature
+    });
+    // Call createMap function with earthquakes 
+    createMap(earthquakes);
+    // console.log(earthquakes);
 
-  L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        tileSize: 512,
-        maxZoom: 10,
-        zoomOffset: -1,      
-        id: "streets-v11",
-        accessToken: API_KEY
-      }).addTo(earthQuakeMap);
+    // Start mapping process
+    function createMap(earthquakes) {
+        
+        // Define streetmap layer
+        var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+            attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+            tileSize: 512,
+            maxZoom: 18,
+            zoomOffset: -1,
+            id: "mapbox/streets-v11",
+            accessToken: API_KEY
+        });
 
-  // // Get request to URL
-  d3.json(url, function(data) {
-    L.geoJSON(data, {
-      pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, markerStyle(feature));
-      },
-    // Call pop-up for each feature
-    onEachFeature: function(feature, layer) {
-      layer.bindPopup("<h1> Magnitude: " + feature.properties.mag+ "</h1> <hr> <h2>" + feature.properties.place + "</h2>");
-      }
-    }).addTo(earthQuakeMap);
+        // Create baseMaps object to hold our base layers
+        var baseMaps = {
+            "Street Map": streetmap
+        };
 
-  mapLegend(earthQuakeMap);
-  
+        // Create overlay object to hold our overlay layer
+        var overlayMaps = {
+            Earthquakes: earthquakes
+        };
 
-  });
+        // Create map with streetmap and earthquakes layers to display when loaded
+        var map = L.map("map", {
+            center: [32.7767, -96.7970],
+            zoom: 4,
+            layers: [streetmap, earthquakes]
+        });
 
-};
+        // Create a layer control. Pass in baseMaps and overlayMaps
+        // Add the layer control to the map
+        L.control.layers(baseMaps, overlayMaps, {
+            collapsed: false
+        }).addTo(map);
 
-//function for creating legend
-function mapLegend (map) {
+        // // Create legend
+        // function for legend colors
+        function getColor(c) {
+            return  c > 90 ? "#bd0026":
+                    c > 70 ? "#f03b20":
+                    c > 50 ? "#fd8d3c":
+                    c > 30 ? "#feb24c":
+                    c > 10 ? "#fed976":
+                            "#ffffb2";
+        }
+        // Set up legend variable to L.control location
+        var legend = L.control({position: "bottomright"});
 
-  colors = ["#459E22", "#7FB20E", "#BEBE02", "#B19A0F", "#B54C0B", "#C00000"];
-
-  var legend = L.control({position: 'bottomright'});
-
-  legend.onAdd = function () {
-
-    var div = L.DomUtil.create('div', 'info legend'),
-                  categories = ['<1', '1 to <2', '2 to <3', '3 to <4', '4 to <5', '>5'],
-                  labels =[];
-    
-    div.innerHTML += '<strong> Magnitude </strong> <br>'
-    // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < categories.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + colors[i] + '"></i> ' +
-            categories[i] + '<br>';
-    };
-    return div;
- };
-legend.addTo(map);
-};
-
-//function for styling markers
-function markerStyle (feature) {
-  return {
-      fillColor: markerColor(feature.properties.mag),
-      radius: 4*feature.properties.mag,
-      weight: 2,
-      opacity: 1,
-      color: markerColor(feature.properties.mag),
-      fillOpacity: 0.8   
-  };
-};
-
-// Function determining the color of marker based on magnitude
-function markerColor(magnitude) {
-  if (magnitude<1) {
-    return "#459E22"}
-  else if (magnitude<2) {
-     return "#7FB20E"}
-  else if (magnitude<3) {
-     return "#BEBE02"}
-  else if (magnitude<4) {
-     return "#B19A0F"}
-  else if (magnitude<5) {
-     return "#B54C0B"}
-  else if (magnitude>=5) {
-     return "#C00000"}
-  else {
-     return "black"}
- };
- 
-mapDisplay();
+        // Set up legend features
+        legend.onAdd = function(map) {
+            // Create div for legend
+            var div = L.DomUtil.create("div", "info legend");
+            // Set legend labels
+            var mags = [-10, 10, 30, 50, 70, 90];
+            var labels = [];
+            // for loop through mag value ranges and use innerHTML to getColor functon for 
+            // color parameters
+            for (var i = 0; i < mags.length; i++) {
+                div.innerHTML +=
+                '<i style="background:' + getColor(mags[i] + 1) + '"></i>' +
+                mags[i] + (mags[i + 1] ? '&ndash;' + mags[i + 1] + '<br>' : '+');
+            }
+            return div;
+        };
+        legend.addTo(map);
+    }
+}
